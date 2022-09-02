9d105f3d5cf41ed16eeb4f305f0c59b1556ed691
Sam Selfridge <sam.selfridge@gmail.com>
2022-09-02 15:00:13 -0700

    Add effort and ActivitySummary models

---
+++ Models/db/Effort.cs
@@ -0,0 +1,34 @@
+﻿using TodoApi.Models.stravaApi;
+
+namespace TodoApi.Models.db
+{
+  public class Effort
+  {
+    public long Id { get; set; }
+    public int AthleteId { get; set; }
+    public long ActivityId { get; set; }
+    public int ElapsedTime { get; set; }
+    public int MovingTime { get; set; }
+
+
+    public Effort(long id, int athleteId, long activityId, int elapsedTime, int movingTime)
+    {
+      Id = id;
+      AthleteId = athleteId;
+      ActivityId = activityId;
+      ElapsedTime = elapsedTime;
+      MovingTime = movingTime;
+    }
+
+    public Effort(ActivitySumResEffort eff)
+    {
+      Id = eff.Id;
+      AthleteId = eff.Athlete.Id;
+      ActivityId = eff.Activity.Id;
+      ElapsedTime = eff.ElapsedTime;
+      MovingTime = eff.MovingTime;
+    }
+  }
+
+
+}
---
+++ Models/stravaApi/ActivitySummaryResponse.cs
@@ -0,0 +1,77 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TodoApi.Models.stravaApi
+{
+  public class ActivitySummaryResponse
+  {
+    [JsonPropertyName("id")]
+    public long? Id { get; set; }
+
+    [JsonPropertyName("athlete")]
+    public ActivitySummaryResAthlete? Athlete { get; set; }
+
+    [JsonPropertyName("start_date")]
+    public string? StartDate { get; set; }
+
+    [JsonPropertyName("private")]
+    public Boolean Private { get; set; }
+
+    [JsonPropertyName("visibility")]
+    public string? Visibility { get; set; }
+
+    [JsonPropertyName("segment_efforts")]
+    public ActivitySumResEffort[] SegmentEfforts { get; set; }
+
+  }
+
+  public class ActivitySummaryResAthlete
+  {
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("resource_state")]
+    public long ResourceState { get; set; }
+  }
+
+  public class ActivitySummaryResActivity
+  {
+    [JsonPropertyName("id")]
+    public long Id { get; set; }
+
+    [JsonPropertyName("resource_state")]
+    public long ResourceState { get; set; }
+  }
+  public class ActivitySumResEffort
+  {
+    [JsonPropertyName("id")]
+    public long Id { get; set; }
+
+    [JsonPropertyName("elapsed_time")]
+    public int ElapsedTime { get; set; }
+
+    [JsonPropertyName("moving_time")]
+    public int MovingTime { get; set; }
+
+    [JsonPropertyName("start_date")]
+    public string? StartDate { get; set; }
+
+    [JsonPropertyName("segment")]
+    public ActivitySumResSegment Segment { get; set; }
+
+    [JsonPropertyName("athlete")]
+    public ActivitySummaryResAthlete Athlete { get; set; }
+    [JsonPropertyName("activity")]
+    public ActivitySummaryResActivity Activity { get; set; }
+  }
+
+  public class ActivitySumResSegment
+  {
+    [JsonPropertyName("id")]
+    public long Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+  }
+
+}
