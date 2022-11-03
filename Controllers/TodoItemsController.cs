c3fe775e78639c21ffea756d0801353abbb46af1
Sam Selfridge <sam.selfridge@gmail.com>
2022-11-03 13:56:33 -0700

    block todo controller on PROD (should really have an ENV check there...)

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -83,7 +83,7 @@ namespace TodoApi.Controllers
     [HttpGet()]
     public async Task<ActionResult<TodoItem>> TestThing([FromServices] IServiceScopeFactory serviceScopeFactory)
     {
-      //return Ok("loaded");
+      return Ok("loaded");
 
 
       var effortList = _userService.GetUserEfforts(19340963);
