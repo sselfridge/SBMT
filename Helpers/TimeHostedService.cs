cc1a43553698288519f5fc0a89b6a42d863121ec
Sam Selfridge <sam.selfridge@gmail.com>
2023-01-26 18:50:54 -0800

    add missing file

---
+++ Helpers/TimeHostedService.cs
@@ -0,0 +1,49 @@
+﻿namespace TodoApi.Helpers
+{
+  public class TimedHostedService : IHostedService, IDisposable
+  {
+    private int executionCount = 0;
+    private readonly ILogger<TimedHostedService> _logger;
+    private IServiceScopeFactory _serviceScopeFactory;
+    private Timer? _timer = null;
+
+    public TimedHostedService(ILogger<TimedHostedService> logger, IServiceScopeFactory serviceScopeFactory)
+    {
+      _logger = logger;
+      _serviceScopeFactory = serviceScopeFactory;
+    }
+
+    public Task StartAsync(CancellationToken stoppingToken)
+    {
+      _logger.LogInformation("Timed Hosted Service running.");
+
+      _timer = new Timer(DoWork, null, TimeSpan.Zero,
+          TimeSpan.FromSeconds(30));
+
+      return Task.CompletedTask;
+    }
+
+    private void DoWork(object? state)
+    {
+      var count = Interlocked.Increment(ref executionCount);
+      StravaUtilities.TestUtilitiesAccess(_serviceScopeFactory);
+      Console.WriteLine($"DoWork{count}");
+      _logger.LogInformation(
+          "Timed Hosted Service is working. Count: {Count}", count);
+    }
+
+    public Task StopAsync(CancellationToken stoppingToken)
+    {
+      _logger.LogInformation("Timed Hosted Service is stopping.");
+
+      _timer?.Change(Timeout.Infinite, 0);
+
+      return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+      _timer?.Dispose();
+    }
+  }
+}
