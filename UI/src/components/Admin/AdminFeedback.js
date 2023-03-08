3e0d0c48a413bd3c77e50759300c8e75fcfb7e33
Sam Selfridge <sam.selfridge@gmail.com>
2023-03-07 21:23:40 -0800

    Add feedback to admin visible

---
+++ UI/src/components/Admin/AdminFeedback.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Box,
+  Avatar,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { ApiGet } from "api/api";
+
+const AdminFeedback = (props) => {
+  const [feedback, setFeedback] = useState([]);
+
+  useEffect(() => {
+    ApiGet("/api/admin/feedback", setFeedback);
+  }, []);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Athlete Id</TableCell>
+            <TableCell align="right">Text</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {feedback.map((f) => (
+            <TableRow
+              key={f.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                <Link to={`/athletes/${f.athleteId}`} key={f.id}>
+                  <Box sx={{ display: "flex", alignItems: "center" }}>
+                    <Avatar src={f.avatar} />
+                    {f.name}
+                  </Box>
+                </Link>
+              </TableCell>
+              <TableCell align="right">{f.text}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+AdminFeedback.propTypes = {
+  prop: PropTypes.object,
+};
+
+export default AdminFeedback;
