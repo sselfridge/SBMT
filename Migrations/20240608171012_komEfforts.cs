82aabc27edd9b45e92b4cd3c04cd9f6ccbc4a9da
Sam Selfridge <sam.selfridge@gmail.com>
2024-06-08 10:10:36 -0700

    prod migration

---
+++ Migrations/20240608171012_komEfforts.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApi.Migrations
+{
+    public partial class komEfforts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "KomRank",
+                table: "Efforts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KomRank",
+                table: "Efforts");
+        }
+    }
+}
