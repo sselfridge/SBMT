3060e879a0fe2da9d80246d9dccda79a8d9bf543
Sam Selfridge <sam.selfridge@gmail.com>
2025-02-10 18:57:53 -0800

    and addd appsetting object

---
+++ Models/AppSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace TodoApi.Models
+{
+  public class AppSettingDTO
+  {
+    public DateTime StatsFetchedAt { get; set; }
+
+    public AppSettingDTO()
+    {
+      StatsFetchedAt = DateTime.UtcNow;
+    }
+
+
+  }
+
+
+
+
+}
