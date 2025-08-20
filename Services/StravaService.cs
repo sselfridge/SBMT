7addc1bdf5ffcf934dc38d61ac1d1612325fe11d
Sam Selfridge <sam.selfridge@gmail.com>
2025-08-20 10:26:09 -0700

    testing RAW json before parsing

--- Services/StravaService.cs
+++ Services/StravaService.cs
@@ -445,7 +445,9 @@ namespace TodoApi.Services
       {
         try
         {
-          T? result = await response.Content.ReadFromJsonAsync<T>();
+          string rawJson = await response.Content.ReadAsStringAsync();
+          Console.WriteLine(rawJson);
+          T? result = JsonSerializer.Deserialize<T>(rawJson);
           var limit = response.Headers.GetValues("X-RateLimit-Limit");
           var usage = response.Headers.GetValues("X-RateLimit-Usage");
 
